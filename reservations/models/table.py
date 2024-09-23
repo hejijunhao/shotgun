88a027e069a241f805652919cbe1ec0c538af55b
hejijunhao <pkhelfried@gmail.com>
2024-09-23 15:04:20 +0100

    Added default values to max_capacity and merged_capacity

--- reservations/models/table.py
+++ reservations/models/table.py
@@ -3,9 +3,12 @@ from django.db import models
 class Table(models.Model):
     restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE, related_name='tables')
     number = models.CharField(max_length=5)
-    capacity = models.IntegerField()
+    default_capacity = models.IntegerField()
+    max_capacity = models.IntegerField(default=4)
+    merged_capacity = models.IntegerField(default=4)
     mergable = models.BooleanField(default=False)
     merged_with = models.ManyToManyField('self', blank=True, symmetrical=True)
+
 
     def merge_with(self, other_table):
         if self.mergable and other_table.mergable and not self.merged_with.exists():
@@ -28,13 +31,12 @@ class Table(models.Model):
         return False
 
     def get_effective_capacity(self):
-        total_capacity = self.capacity
         if self.merged_with.exists():
-            total_capacity += sum(table.capacity for table in self.merged_with.all())
-        return total_capacity
+            return self.merged_capacity
+        return self.default_capacity
 
     def can_accommodate(self, party_size):
-        return party_size <= self.get_effective_capacity()
+        return self.default_capacity <= party_size <= self.max_capacity
 
     def get_merged_identifier(self):
         # Create a unique identifier for merged tables by concatenating their numbers
@@ -50,3 +52,11 @@ class Table(models.Model):
     class Meta:
         unique_together = ['restaurant', 'number']
         ordering = ['number']
+
+    def get_effective_capacity(self):
+        if self.merged_with.exists():
+            return self.merged_capacity
+        return self.default_capacity
+
+    def can_accommodate(self, party_size):
+        return self.default_capacity <= party_size <= self.max_capacity
