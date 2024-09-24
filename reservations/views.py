1c79c6f5851e8cb774ccd1fc62a4db584248795a
hejijunhao <pkhelfried@gmail.com>
2024-09-24 13:33:13 +0100

    improvements to calendar ui

--- reservations/views.py
+++ reservations/views.py
@@ -49,66 +49,39 @@ def create_reservation(request):
 
 def restaurant_calendar(request):
     # Get all tables
-    tables = Table.objects.all()
+    tables = list(Table.objects.all().values('id', 'number'))
 
-    # Define the date for which to display reservations (e.g., today or selected date)
+    # Get reservations for today or for a selected date
     date_str = request.GET.get('date')
     if date_str:
-        date = datetime.strptime(date_str, '%Y-%m-%d').date()
+        selected_date = datetime.strptime(date_str, '%Y-%m-%d').date()
     else:
-        date = datetime.now().date()
+        selected_date = datetime.now().date()
 
-    # Generate time slots (e.g., from 10:00 AM to 10:00 PM in 30-minute intervals)
-    start_time = datetime.combine(date, datetime.strptime('10:00', '%H:%M').time())
-    end_time = datetime.combine(date, datetime.strptime('22:00', '%H:%M').time())
-    time_slots = []
-    current_time = start_time
-    while current_time < end_time:
-        time_slots.append(current_time)
-        current_time += timedelta(minutes=30)
-
-    # Get reservations for the selected date
-    reservations = Reservation.objects.filter(
-        start_datetime__date=date
-    ).select_related('guest').prefetch_related('tables')
-
-    # Convert tables to a list of dictionaries for serialization
-    tables_data = list(tables.values('id', 'number'))
-
-    # Map reservations to tables and time slots
-    reservation_map = {}
-    for reservation in reservations:
-        res_start = reservation.start_datetime
-        res_end = reservation.end_datetime
-        res_times = []
-        current_res_time = res_start
-        while current_res_time < res_end:
-            res_times.append(current_res_time)
-            current_res_time += timedelta(minutes=30)
-        for table in reservation.tables.all():
-            for res_time in res_times:
-                key = (table.id, res_time.strftime('%H:%M'))
-                reservation_map[key] = reservation
-
-    # Serialize reservations for JavaScript
-    serialized_reservations = json.dumps(
-        list(reservations.values(
-            'id',
-            'start_datetime',
-            'end_datetime',
-            guest_name=F('guest__name'),
-            tables_numbers=ArrayAgg('tables__number'),
-        )),
-        cls=DjangoJSONEncoder
+    reservations = list(
+        Reservation.objects.filter(
+            start_datetime__date=selected_date
+        ).select_related('guest').values(
+            'id', 'guest__name', 'start_datetime', 'end_datetime', 'tables__number'
+        )
     )
 
+    # Generate time slots (e.g., from 10:00 AM to 10:00 PM in 30-minute intervals)
+    time_slots = []
+    start_time = datetime.combine(selected_date, datetime.strptime('10:00', '%H:%M').time())
+    end_time = datetime.combine(selected_date, datetime.strptime('22:00', '%H:%M').time())
+    current_time = start_time
+    while current_time < end_time:
+        time_slots.append(current_time.strftime('%H:%M'))
+        current_time += timedelta(minutes=30)
+
     context = {
-        'tables': tables_data,  # Pass the serialized table data
+        'tables': tables,
         'time_slots': time_slots,
-        'reservation_map': reservation_map,
-        'reservations_json': serialized_reservations,
-        'selected_date': date,
+        'reservations': json.dumps(reservations, cls=DjangoJSONEncoder),
+        'selected_date': selected_date,
     }
+
     return render(request, 'reservations/restaurant_calendar.html', context)
 
 # If you still need the API endpoints
